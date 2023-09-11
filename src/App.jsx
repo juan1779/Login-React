63011dad4f40a03ebbbdae0b81aa2abf72c34093
Juan <santiljuan179@gmail.com>
2023-09-11 13:13:32 -0400

    agregando rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./app/pages/Home/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Profile from "./app/pages/Profile/Profile";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/app/pages/Home/Home.jsx
+++ src/app/pages/Home/Home.jsx
@@ -6,6 +6,10 @@ import Profile from "../Profile/Profile";
 import { useSelector } from "react-redux";
 
 function Home() {
+  const [miLogin, setMiLogin] = useState(false);
+  //info redux
+  const users = useSelector((state) => state.users.users);
+
   //Modal state
   const [open, setOpen] = useState(false);
   const handleOpen = (e) => {
@@ -13,51 +17,59 @@ function Home() {
   };
   const handleClose = () => setOpen(false);
 
-  //LoginState
+  //LoginState Email
 
-  const [dataLogin, setDataLogin] = useState({
-    email: "",
-    password: "",
-  });
+  const [dataLoginEmail, setDataLoginEmail] = useState(null);
 
-  const users = useSelector((state) => state.users.users);
-
-  const handleChange = (e) => {
-    setDataLogin({ [e.target.name]: e.target.value });
+  const handleChangeEmail = (e) => {
+    setDataLoginEmail(e.target.value);
   };
+
+  //LoginState Password
+  const [dataLoginPassword, setDataLoginPassword] = useState(null);
+
+  const handleChangePassword = (e) => {
+    setDataLoginPassword(e.target.value);
+  };
+
+  //function submit info
 
   const onSubmit = (e) => {
     e.preventDefault();
+
     const userValidation = users.find(
       (user) =>
-        user.email === dataLogin.email && user.password === dataLogin.password
+        user.email === dataLoginEmail && user.password === dataLoginPassword
     );
 
     if (userValidation) {
+      setMiLogin(true);
       alert("login");
+      document.getElementById("form_login").style.display = "none";
     } else {
-      alert("fallo en registro");
+      alert("incorrect credentials");
     }
   };
 
   return (
     <div className="container">
-      <form className="formLogin">
+      <form id="form_login" className="formLogin">
         <img className="imgLogin" src={img} alt="image" />
 
         <input
           type="email"
           name="email"
           placeholder="Email"
-          onChange={handleChange}
+          onChange={handleChangeEmail}
           required
+          autoFocus
         />
 
         <input
           type="password"
           name="password"
           placeholder="Password"
-          onChange={handleChange}
+          onChange={handleChangePassword}
           required
         />
         <div>
@@ -75,6 +87,7 @@ function Home() {
       </form>
 
       <Modals open={open} onClose={handleClose} />
+      {miLogin === true && <Profile />}
     </div>
   );
 }
